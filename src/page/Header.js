52dd28868645b4970ffd3d3adc4fb75539c51dc1
pamphilemkp <pamphilemkp@gmail.com>
2022-09-03 15:40:55 +0300

    add stylint

--- src/page/Header.js
+++ src/page/Header.js
@@ -3,8 +3,11 @@ import { Link } from 'react-router-dom';
 function Header() {
   return (
     <nav className="header">
-      <Link to="/"> Books </Link>
-      <Link to="/Categories"> Categories </Link>
+      <div className="nav">
+        <h1 className="Bookstore-cms">Bookstore CMS</h1>
+        <Link to="/" className="book-link"> Books </Link>
+        <Link to="/Categories" className="category-link"> Categories </Link>
+      </div>
     </nav>
   );
 }
