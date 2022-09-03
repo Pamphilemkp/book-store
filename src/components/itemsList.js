f74966c3b01afae3199dd2c2d6816ffa8b184fa1
pamphilemkp <pamphilemkp@gmail.com>
2022-09-03 03:08:50 +0300

    add book and get book to api

--- src/components/itemsList.js
+++ src/components/itemsList.js
@@ -1,17 +1,21 @@
 /* eslint-disable react/button-has-type */
 /* eslint-disable react/prop-types */
 /* eslint-disable react/destructuring-assignment */
+// const apiURl = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/IwD6i2qVfkciDWYsseU9/books';
+
 function ItemsList(props) {
   return (
     <ul className="list">
       {props.books.map((data) => (
-        <li key={data.id} className="list-items">
+        <li key={data.item_id} className="list-items">
           <p>
             {data.author}
             :
             <span>{data.title}</span>
+            { }
+            <span>{data.category}</span>
           </p>
-          <button onClick={() => props.bookRemove(data.id)}>delete</button>
+          <button onClick={() => props.bookRemove(data.item_id)}>delete</button>
         </li>
       ))}
     </ul>
